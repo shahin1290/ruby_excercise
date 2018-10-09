2448c7f7a99d2908cc6068838bda45b0a64bdfd6
Md Shahin Patowary <s.patowary@yahoo.com>
2018-10-09 21:36:37 +0200

    added basics_2

---
+++ basics_2.rb
@@ -0,0 +1,13 @@
+movie_year = {
+    'jaws' => 1975,
+    'Raiders of the Lost Ark' => 1981,
+    'A Beautiful Mind' => 2001,
+    'The Notebook' => 2004,
+    'Frozen' => 2013
+}
+
+p movie_year['jaws']
+p movie_year['The Notebook']
+p movie_year['Frozen']
+p movie_year['A Beautiful Mind']
+p movie_year['Raiders of the Lost Ark']
